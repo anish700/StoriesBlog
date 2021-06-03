c7538bf20fa6cc679295c72340e93a1161b5f21d
anish <anish.ravichandran@gmail.com>
2021-06-03 23:55:20 +0530

    commiting to github

---
+++ middleware/auth.js
@@ -0,0 +1,18 @@
+//to not allow anyone to access /dashboard etc without logging in
+
+module.exports = {
+  ensureAuth: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      return next();
+    } else {
+      res.redirect("/");
+    }
+  },
+  ensureGuest: function (req, res, next) {
+    if (req.isAuthenticated()) {
+      res.redirect("/dashboard");
+    } else {
+      return next();
+    }
+  },
+};
